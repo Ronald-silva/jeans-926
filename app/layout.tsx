2e1a4e48609ca3230be0d9a7cf790ce4d3fe7830
Ronald-silva <ronaldsilvajob@yahoo.com>
2025-03-19 11:54:18 -0300

    ajuste no build

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,15 @@ export const metadata = {
   title: "Jeans_926 - Moda Feminina com Estilo e Qualidade",
   description:
     "Loja virtual de roupas femininas com variedade de peças e tamanhos. Calças jeans, shorts, saias e blusas para todos os estilos.",
+  icons: {
+    icon: [
+      {
+        url: "/favicon.svg",
+        type: "image/svg+xml",
+      }
+    ],
+    shortcut: ["/favicon.svg"],
+  },
 }
 
 export default function RootLayout({
